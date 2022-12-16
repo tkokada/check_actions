493934537316d432e48ebd261943ef0746eb5f45
Takashi OKADA <tk.okada.github@gmail.com>
2022-12-16 10:54:28 +0900

    update (#1)

--- tests/test_ca.py
+++ tests/test_ca.py
@@ -1,6 +1,11 @@
 from check_actions.ca import simple_add
+from check_actions.ca import simple_multi
 
 
 def test_simple_add():
     assert simple_add(1, 2) == 3
+   
+ 
+def test_simple_multi():
+    assert simple_multi(1, 2) == 2
     
